b7fb3649483a1ecdfe8743b37e5b8c742ad52087
Kasper Zederkof <k.zederkof@cognigy.com>
2019-07-31 15:49:09 +0200

    Use request-promise

--- fetch-cert.js
+++ fetch-cert.js
@@ -1,41 +1,45 @@
-'use strict'
+'use strict';
 
-var https = require('https')
+const request = require('request-promise');
 
 
-var globalCache = {} // default in-memory cache for downloaded certificates
+const globalCache = {} // default in-memory cache for downloaded certificates;
 
-module.exports = function fetchCert (options, callback) {
-  var url = options.url
-  var cache = options.cache || globalCache
-  var cachedResponse = cache[url.href]
-  var servedFromCache = false
-  if (cachedResponse) {
-    servedFromCache = true
-    process.nextTick(callback, undefined, cachedResponse, servedFromCache)
-    return
-  }
+module.exports = async function fetchCert(options, callback) {
+    const url = options.url;
+    const cache = options.cache || globalCache;
+    const cachedResponse = cache[url.href];
+    const servedFromCache = false;
 
-  var body = ''
-
-  https.get(url.href, function (response) {
-    var statusCode
-
-    if (!response || 200 !== response.statusCode) {
-      statusCode = response ? response.statusCode : 0
-      return callback('Failed to download certificate at: ' + url.href + '. Response code: ' + statusCode)
+    if (cachedResponse) {
+        servedFromCache = true;
+        process.nextTick(callback, undefined, cachedResponse, servedFromCache);
+        return;
     }
 
-    response.setEncoding('utf8')
-    response.on('data', function (chunk) {
-      body += chunk
-    })
-    response.on('end', function () {
-      cache[url.href] = body
-      callback(undefined, body, servedFromCache)
-    })
-  })
-  .on('error', function(er) {
-    callback('Failed to download certificate at: ' + url.href +'. Error: ' + er)
-  })
+    try {
+        /**
+         * Use the request-promise library
+         * to fetch the certificate, since
+         * this library honors the HTTP(S)_PROXY
+         * env variables
+         */
+        const response = await request(url, {
+            json: true,
+            resolveWithFullResponse: true
+        });
+
+        if (!response || 200 !== response.statusCode) {
+            statusCode = response ? response.statusCode : 0;
+            return callback('Failed to download certificate at: ' + url.href + '. Response code: ' + statusCode);
+        }
+
+        const body = response.body;
+
+        cache[url.href] = body;
+        callback(undefined, body, servedFromCache);
+
+    } catch (err) {
+        callback('Failed to download certificate at: ' + url.href + '. Error: ' + err);
+    }
 }
